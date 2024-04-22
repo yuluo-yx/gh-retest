e95bbf3f6ab796fc4f7d1d2917bdc45c263a3422
yuluo-yx <yuluo08290126@gmail.com>
2024-04-22 12:37:09 +0800

    test

--- types.ts
+++ types.ts
@@ -11,6 +11,7 @@ type GithubReactionType =
   | "heart"
   | "hooray"
   | "eyes";
+  
 type CheckRunsType =
   Endpoints["GET /repos/{owner}/{repo}/commits/{ref}/check-runs"]["response"];
 type CreateReactionType =
